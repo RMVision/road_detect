be177b49e20131620108cd3182c16c2aee4b67c8
ZhanZongHan <347649372@qq.com>
2018-12-17 21:25:45 +0800

    debug opencv

--- main.cpp
+++ main.cpp
@@ -3,12 +3,20 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
-
+using namespace cv;
 int main(){
+    VideoCapture capture(0);
+    Mat frame;
+    while(1) {
+        capture >> frame;
+        imshow("frame", frame);
+        if (waitKey(1) == 'q')
+            break;
 
-    cout << "Hello World!!!" << endl;
-
+    }
+    destroyWindow("frame");
     return 0;
 }
