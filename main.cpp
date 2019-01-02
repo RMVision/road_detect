b3640486d9b486d69730c90d6854d999d2ffee85
niracler <niracler@niracler-pc>
2019-01-02 19:08:55 +0800

    封装函数

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ static int contrast = 255;
 int max_thres = 255;
 int val = 1;
 Mat canny_white;
-Mat white_mask, yellow_mask;
 
 void cannyCallBack(int, void *);
 Mat getWarpPerspective(const Mat &input);
@@ -33,13 +32,17 @@ Mat imgEnhancement(const Mat &input);
 
 Mat imgMask(const Mat &input);
 
+Mat edgeDetector(const Mat &input);
+
+Mat imgMorphological(const Mat &input);
+
 int main() {
 
     VideoCapture capture(path);
     Mat frame;
     Mat resize_frame, auto_frame, adjust_frame, denoise_frame;
-    Mat enhancement_frame;
-    Mat warp;
+    Mat enhancement_frame, morphological_frame;
+    Mat warp, edges_frame;
     Mat mask_frame;
 
     int width = (int) capture.get(3);
@@ -59,31 +62,28 @@ int main() {
         //亮度及对比度自适应
         BrightnessAndContrastAuto(resize_frame, auto_frame, 5);
         adjustBrightnessContrast(auto_frame, adjust_frame, brightness - 255, contrast - 255);
-        imshow("亮度及对比度自适应结果", adjust_frame);
+//        imshow("亮度及对比度自适应结果", adjust_frame);
 
         //图像增强
         enhancement_frame = imgEnhancement(adjust_frame);
 
         //进行模糊化处理
-        denoise_frame = deNoise(enhancement_frame);
+//        denoise_frame = deNoise(enhancement_frame);
 
         //TODO:这里应该有个形态学变换
+        morphological_frame = imgMorphological(enhancement_frame);
 
         //透视变换
-        warp = getWarpPerspective(denoise_frame);
-        imshow("经过透视变换的道路图", warp);
+        warp = getWarpPerspective(morphological_frame);
+//        imshow("经过透视变换的道路图", warp);
 
         //标记黄色与白色
         mask_frame = imgMask(warp);
         imshow("标记黄色与白色", mask_frame);
 
-
-//        imshow("白色1", white_mask);
-        //canny边缘检测
-//        blur(white_mask, white_mask, Size(5, 5));
-///*        createTrackbar("canny", "white", &val, max_thres, canny_call_back);
-//        canny_call_back(0, 0);*/
-//        Canny(white_mask, canny_white, val, val * 2);
+        //边缘检测
+        edges_frame = edgeDetector(mask_frame);
+        imshow("进行边缘检测", edges_frame);
 
         // roi Hough直线检测
  //       roi_mat = canny_white(roi);
@@ -99,6 +99,7 @@ int main() {
 
         imshow("dst", dst);
         imshow("copy_mask", re_warp);
+
  /*       double k_queue[lines.size()];
         int p1_ys[lines.size()];
         int p2_xs[lines.size()];
@@ -126,10 +127,7 @@ int main() {
 
         line(resize_frame(roi), p1, p2, Scalar(0, 0, 255), 5);
         imshow("frame", resize_frame);
-        imshow("roi_mat", roi_mat);
-        imshow("white", canny_white);
-        imshow("白色", white_mask);
-        imshow("黄色", yellow_mask);*/
+*/
 
         if (waitKey(20) == 'q')
             break;
@@ -138,9 +136,23 @@ int main() {
 
 }
 
+Mat imgMorphological(const Mat &input) {
+    return input;
+}
+
+Mat edgeDetector(const Mat &input) {
+    Mat output;
+    //canny
+    blur(input, input, Size(5, 5));
+    Canny(input, output, val, val * 2);
+
+    return output;
+}
+
 Mat imgMask(const Mat &input) {
 
     // 设置阈值，挑出白色与黄色区域
+    static Mat white_mask, yellow_mask;
     static Vec3i white_low(0, 180, 0), yellow_low(26, 43, 46);
     static Vec3i white_top(255, 255, 255), yellow_top(34, 255, 255);
     Mat output;
@@ -170,10 +182,10 @@ Mat deNoise(const Mat &input) {
     return output;
 }
 
-void cannyCallBack(int, void *) {
-    Canny(white_mask, canny_white, val, val * 2);
-    imshow("white", canny_white);
-}
+//void cannyCallBack(int, void *) {
+//    Canny(white_mask, canny_white, val, val * 2);
+//    imshow("white", canny_white);
+//}
 
 Mat getWarpPerspective(const Mat &input) {
     Point2f pts_src[] = {
